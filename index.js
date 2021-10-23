51012da00f8725af126626c2a9faa381c9803f55
Joshua Ordehi <mev.dev.kar@gmail.com>
2021-10-23 17:04:41 -0400

    add scraping specific newspaper

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const newspapers = [
     base: 'https://www.thetimes.co.uk',
   },
   {
-    name: 'guardian',
+    name: 'theguardian',
     address: 'https://www.theguardian.com/environment/climate-crisis',
     base: 'https://www.theguardian.com',
   },
@@ -53,6 +53,37 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
   res.json(articles);
+});
+
+app.get('/news/:newspaperId', async (req, res) => {
+  const newspaperId = req.params.newspaperId;
+
+  const newspaperAddress = newspapers.filter(
+    (newspaper) => newspaper.name == newspaperId
+  )[0].address;
+  const newspaperBase = newspapers.filter(
+    (newspaper) => newspaper.name == newspaperId
+  )[0].base;
+
+  axios
+    .get(newspaperAddress)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const specificArticles = [];
+
+      $('a:contains("climate")', html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr('href');
+        specificArticles.push({
+          title,
+          url: url.indexOf(newspaperBase) != -1 ? url : newspaperBase + url,
+          source: newspaperId,
+        });
+      });
+      res.json(specificArticles);
+    })
+    .catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
