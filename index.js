4d36fa8f2fdfecf265b726e98f01a0ccea9a7b48
Joshua Ordehi <mev.dev.kar@gmail.com>
2021-10-23 16:17:12 -0400

    add basic single site scraping functionality

--- index.js
+++ index.js
@@ -5,8 +5,32 @@ const cheerio = require('cheerio');
 
 const app = express();
 
+const articles = [];
+
 app.get('/', (req, res) => {
   res.json('Welcome to my test API');
+});
+
+app.get('/news', (req, res) => {
+  axios
+    .get('https://www.theguardian.com/environment/climate-crisis')
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      $('a:contains("climate")', html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr('href');
+        articles.push({
+          title,
+          url,
+        });
+      });
+      res.json(articles);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
